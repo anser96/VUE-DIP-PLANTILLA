b9dfff1eb234858a92fed732053e18c6c0e6d432
JuanCAlzate <144971492+JuanCAlzate@users.noreply.github.com>
2024-11-12 15:22:26 -0500

    Registro de tarea por modulos

--- src/modules/members/MembersCreate.vue
+++ src/modules/members/MembersCreate.vue
@@ -1,38 +1,56 @@
 <template>
   <div class="p-4">
     <h1 class="text-3xl font-bold mb-4">
-      {{ isEditing ? "Editar Miembro" : "Crear Nuevo Miembro" }}
+      {{ isEditing ? 'Editar Miembro' : 'Crear Nuevo Miembro' }}
     </h1>
 
     <form @submit.prevent="submitForm" class="space-y-4">
       <!-- Campo para el Nombre -->
       <div>
         <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre</label>
-        <input type="text" id="nombre" v-model="newMember.nombre" class="input input-bordered w-full"
-          placeholder="Ingrese el nombre" required />
+        <input
+          type="text"
+          id="nombre"
+          v-model="newMember.nombre"
+          class="input input-bordered w-full"
+          placeholder="Ingrese el nombre"
+          required
+        />
         <p v-if="errors.nombre" class="text-red-500 text-sm mt-1">{{ errors.nombre }}</p>
       </div>
 
       <!-- Campo para el Cargo -->
       <div>
         <label for="cargo" class="block text-sm font-medium text-gray-700">Cargo</label>
-        <input type="text" id="cargo" v-model="newMember.cargo" class="input input-bordered w-full"
-          placeholder="Ingrese el cargo" required />
+        <input
+          type="text"
+          id="cargo"
+          v-model="newMember.cargo"
+          class="input input-bordered w-full"
+          placeholder="Ingrese el cargo"
+          required
+        />
         <p v-if="errors.cargo" class="text-red-500 text-sm mt-1">{{ errors.cargo }}</p>
       </div>
 
       <!-- Campo para el Email -->
       <div>
         <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-        <input type="email" id="email" v-model="newMember.email" class="input input-bordered w-full"
-          placeholder="Ingrese el email" required />
+        <input
+          type="email"
+          id="email"
+          v-model="newMember.email"
+          class="input input-bordered w-full"
+          placeholder="Ingrese el email"
+          required
+        />
         <p v-if="errors.email" class="text-red-500 text-sm mt-1">{{ errors.email }}</p>
       </div>
 
       <!-- Botón de Crear o Actualizar -->
       <div class="flex justify-end">
         <button type="submit" class="btn btn-primary">
-          {{ isEditing ? "Actualizar Miembro" : "Crear Miembro" }}
+          {{ isEditing ? 'Actualizar Miembro' : 'Crear Miembro' }}
         </button>
       </div>
     </form>
@@ -40,103 +58,105 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import { useRouter, useRoute } from "vue-router";
-import { Miembro, ApiResponse } from "../../Utils/Interfaces/MeetingRecords";
-import { createMiembro, getMiembroById, updateMiembro } from "../../services/miembroService";
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { Miembro, ApiResponse } from '../../Utils/Interfaces/MeetingRecords'
+import { createMiembro, getMiembroById, updateMiembro } from '../../services/miembroService'
 
 // Inicializar el nuevo miembro
 const newMember = ref<Miembro>({
-  nombre: "",
-  cargo: "",
-  email: "",
+  nombre: '',
+  cargo: '',
+  email: '',
   idMiembro: 0,
-  asistenciaMiembros: []
-});
+  asistenciaMiembros: [],
+})
 
 const errors = ref({
   nombre: '',
   cargo: '',
-  email: ''
-});
+  email: '',
+})
 
-const router = useRouter();
-const route = useRoute();
-const isEditing = ref(false);
-const memberId = ref<number | null>(null);
+const router = useRouter()
+const route = useRoute()
+const isEditing = ref(false)
+const memberId = ref<number | null>(null)
 
 // Si es modo edición, cargar los datos del miembro existente
 onMounted(async () => {
-  memberId.value = route.params.id ? parseInt(route.params.id as string, 10) : null;
-  isEditing.value = memberId.value !== null;
+  memberId.value = route.params.id ? parseInt(route.params.id as string, 10) : null
+  isEditing.value = memberId.value !== null
 
   if (isEditing.value && memberId.value !== null) {
     try {
-      const response: ApiResponse<Miembro> = await getMiembroById(memberId.value);
-      if (response.status === "success" && response.data) {
-        newMember.value = response.data; // Cargar datos para la edición
+      const response: ApiResponse<Miembro> = await getMiembroById(memberId.value)
+      if (response.status === 'success' && response.data) {
+        newMember.value = response.data // Cargar datos para la edición
       } else {
-        throw new Error(response.message || "Error desconocido al cargar el miembro");
+        throw new Error(response.message || 'Error desconocido al cargar el miembro')
       }
     } catch (error) {
-      console.error("Error al cargar el miembro:", error);
+      console.error('Error al cargar el miembro:', error)
     }
   }
-});
+})
 
 // Validar campos del formulario
 const validateFields = () => {
-  let isValid = true;
-  Object.keys(errors.value).forEach(key => {
-    errors.value[key as keyof typeof errors.value] = '';
-  });
+  let isValid = true
+  Object.keys(errors.value).forEach((key) => {
+    errors.value[key as keyof typeof errors.value] = ''
+  })
 
   if (!newMember.value.nombre) {
-    errors.value.nombre = 'El nombre es obligatorio.';
-    isValid = false;
+    errors.value.nombre = 'El nombre es obligatorio.'
+    isValid = false
   }
 
   if (!newMember.value.cargo) {
-    errors.value.cargo = 'El cargo es obligatorio.';
-    isValid = false;
+    errors.value.cargo = 'El cargo es obligatorio.'
+    isValid = false
   }
 
   if (!newMember.value.email) {
-    errors.value.email = 'El email es obligatorio.';
-    isValid = false;
+    errors.value.email = 'El email es obligatorio.'
+    isValid = false
   }
 
-  return isValid;
-};
+  return isValid
+}
 
 // Manejar la creación y actualización del miembro
 const submitForm = async () => {
   if (!validateFields()) {
-    return;
+    return
   }
 
   try {
-    let response: ApiResponse<Miembro>;
+    let response: ApiResponse<Miembro>
 
     if (isEditing.value && memberId.value !== null) {
       // Actualizar miembro existente
-      response = await updateMiembro(memberId.value, newMember.value);
+      response = await updateMiembro(memberId.value, newMember.value)
     } else {
       // Crear nuevo miembro
-      response = await createMiembro(newMember.value);
-      alert("El miembro se ha registrado exitosamente."); // Muestra la alerta
+      response = await createMiembro(newMember.value)
+      alert('El miembro se ha registrado exitosamente.') // Muestra la alerta
     }
 
-    if (response.status === "success") {
+    console.log(response)
+
+    if (response !== null && response !== undefined) {
       // Redirigir a la tabla de miembros después de la operación exitosa
-      router.push("/members");
+      router.push('/members')
     } else {
-      console.error("Error al guardar el miembro:", response.message);
+      console.error('Error al guardar el miembro:', response.message)
     }
   } catch (error) {
-    console.error("Error al guardar el miembro:", error);
+    console.error('Error al guardar el miembro:', error)
   }
-};
+}
 </script>
 
 <style scoped>
--- src/services/tareaServices.ts
+++ src/services/tareaServices.ts
@@ -1,33 +1,33 @@
 // src/services/tareaService.ts
 
-import { fetchWithAuth } from "../Utils/FetchWithToken";
-import { ApiResponse, Tarea } from "../Utils/Interfaces/MeetingRecords";
+import { fetchWithAuth } from '../Utils/FetchWithToken'
+import { ApiResponse, Tarea } from '../Utils/Interfaces/MeetingRecords'
 
-const API_TAREAS_URL = `${import.meta.env.VITE_API_URL}/tareas`;
+const API_TAREAS_URL = `${import.meta.env.VITE_API_URL}/tareas`
 
 // Obtener todas las tareas con responsables
 export const getTareas = async (): Promise<ApiResponse<Tarea[]>> => {
   try {
-    const response = await fetchWithAuth(API_TAREAS_URL, { method: 'GET' });
-    if (!response.ok) throw new Error('Error al obtener las tareas');
-    return await response.json() as ApiResponse<Tarea[]>;
+    const response = await fetchWithAuth(API_TAREAS_URL, { method: 'GET' })
+    if (!response.ok) throw new Error('Error al obtener las tareas')
+    return (await response.json()) as ApiResponse<Tarea[]>
   } catch (error) {
-    console.error('Error al obtener las tareas:', error);
-    throw error;
+    console.error('Error al obtener las tareas:', error)
+    throw error
   }
-};
+}
 
 // Obtener una tarea por ID
 export const getTareaById = async (id: number): Promise<ApiResponse<Tarea>> => {
   try {
-    const response = await fetchWithAuth(`${API_TAREAS_URL}/${id}`, { method: 'GET' });
-    if (!response.ok) throw new Error(`Error al obtener la tarea con ID: ${id}`);
-    return await response.json() as ApiResponse<Tarea>;
+    const response = await fetchWithAuth(`${API_TAREAS_URL}/${id}`, { method: 'GET' })
+    if (!response.ok) throw new Error(`Error al obtener la tarea con ID: ${id}`)
+    return (await response.json()) as ApiResponse<Tarea>
   } catch (error) {
-    console.error(`Error al obtener la tarea con ID ${id}:`, error);
-    throw error;
+    console.error(`Error al obtener la tarea con ID ${id}:`, error)
+    throw error
   }
-};
+}
 
 // Crear una nueva tarea
 export const createTarea = async (tarea: Partial<Tarea>): Promise<ApiResponse<Tarea>> => {
@@ -36,20 +36,65 @@ export const createTarea = async (tarea: Partial<Tarea>): Promise<ApiResponse<Ta
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(tarea),
-    });
+    })
 
     if (!response.ok) {
-      const errorData = await response.json();
-      const backendMessage = errorData.message || 'Error al crear la tarea';
-      throw new Error(backendMessage);
+      const errorData = await response.json()
+      const backendMessage = errorData.message || 'Error al crear la tarea'
+      throw new Error(backendMessage)
     }
 
-    return await response.json() as ApiResponse<Tarea>;
+    return (await response.json()) as ApiResponse<Tarea>
   } catch (error) {
-    console.error('Error al crear la tarea:', error);
-    throw error;
+    console.error('Error al crear la tarea:', error)
+    throw error
   }
-};
+}
+
+export const asignarTareaNueva = async (tarea: unknown): Promise<ApiResponse<Tarea>> => {
+  const queryParamsString = new URLSearchParams(tarea as Record<string, string>).toString()
+  console.log(queryParamsString)
+  try {
+    const response = await fetchWithAuth(`${API_TAREAS_URL}/asignar?${queryParamsString}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    if (!response.ok) {
+      const errorData = await response.json()
+      const backendMessage = errorData.message || 'Error al crear la tarea'
+      throw new Error(backendMessage)
+    }
+    const data = await response.json()
+    return { status: 'success', message: '', data } as ApiResponse<Tarea>
+  } catch (error) {
+    console.error('Error al crear la tarea:', error)
+    throw error
+  }
+}
+
+export const asignarTareaExistente = async (tarea: Tarea): Promise<ApiResponse<Tarea>> => {
+  const queryParamsString = `tareaId=${tarea.idTarea}&responsableId=${tarea.responsableId}&tipoResponsable=${tarea.tipoResponsable}`
+  console.log(queryParamsString)
+  try {
+    const response = await fetchWithAuth(`${API_TAREAS_URL}/${tarea.idTarea}/asignar?${queryParamsString}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    if (!response.ok) {
+      const errorData = await response.json()
+      const backendMessage = errorData.message || 'Error al crear la tarea'
+      throw new Error(backendMessage)
+    }
+    const data = await response.json()
+    console.log(data)
+    return { status: 'success', message: '', data } as ApiResponse<Tarea>
+  } catch (error) {
+    console.error('Error al crear la tarea:', error)
+    throw error
+  }
+}
 
 // Actualizar una tarea existente
 export const updateTarea = async (id: number, tarea: Partial<Tarea>): Promise<ApiResponse<Tarea>> => {
@@ -58,20 +103,20 @@ export const updateTarea = async (id: number, tarea: Partial<Tarea>): Promise<Ap
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(tarea),
-    });
+    })
 
     if (!response.ok) {
-      const errorData = await response.json();
-      const backendMessage = errorData.message || 'Error al actualizar la tarea';
-      throw new Error(backendMessage);
+      const errorData = await response.json()
+      const backendMessage = errorData.message || 'Error al actualizar la tarea'
+      throw new Error(backendMessage)
     }
 
-    return await response.json() as ApiResponse<Tarea>;
+    return (await response.json()) as ApiResponse<Tarea>
   } catch (error) {
-    console.error('Error al actualizar la tarea:', error);
-    throw error;
+    console.error('Error al actualizar la tarea:', error)
+    throw error
   }
-};
+}
 
 // Actualizar el estado de una tarea
 export const updateEstadoTarea = async (id: number, estado: string): Promise<ApiResponse<Tarea>> => {
@@ -80,29 +125,29 @@ export const updateEstadoTarea = async (id: number, estado: string): Promise<Api
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ estado }),
-    });
+    })
 
     if (!response.ok) {
-      const errorData = await response.json();
-      const backendMessage = errorData.message || 'Error al actualizar el estado de la tarea';
-      throw new Error(backendMessage);
+      const errorData = await response.json()
+      const backendMessage = errorData.message || 'Error al actualizar el estado de la tarea'
+      throw new Error(backendMessage)
     }
 
-    return await response.json() as ApiResponse<Tarea>;
+    return (await response.json()) as ApiResponse<Tarea>
   } catch (error) {
-    console.error('Error al actualizar el estado de la tarea:', error);
-    throw error;
+    console.error('Error al actualizar el estado de la tarea:', error)
+    throw error
   }
-};
+}
 
 // Eliminar una tarea
 export const deleteTarea = async (id: number): Promise<ApiResponse<null>> => {
   try {
-    const response = await fetchWithAuth(`${API_TAREAS_URL}/${id}`, { method: 'DELETE' });
-    if (!response.ok) throw new Error('Error al eliminar la tarea');
-    return await response.json() as ApiResponse<null>;
+    const response = await fetchWithAuth(`${API_TAREAS_URL}/${id}`, { method: 'DELETE' })
+    if (!response.ok) throw new Error('Error al eliminar la tarea')
+    return (await response.json()) as ApiResponse<null>
   } catch (error) {
-    console.error('Error al eliminar la tarea:', error);
-    throw error;
+    console.error('Error al eliminar la tarea:', error)
+    throw error
   }
-};
+}
