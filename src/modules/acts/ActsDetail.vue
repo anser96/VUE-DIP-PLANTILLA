fba6346309af1c898888c670d45e2e043643ec47
Andres Saenz <asaencordero@gmail.com>
2024-11-26 17:31:23 -0500

    ajustes varios

--- src/modules/acts/ActsDetail.vue
+++ src/modules/acts/ActsDetail.vue
@@ -13,8 +13,7 @@
       <button :class="{ 'step-primary': currentStep >= 4 }" class="step">Orden del Día</button>
       <button :class="{ 'step-primary': currentStep >= 5 }" class="step">Solicitudes</button>
       <button :class="{ 'step-primary': currentStep >= 6 }" class="step">Tareas o Compromisos</button>
-      <button :class="{ 'step-primary': currentStep >= 7 }" class="step">Próxima Sesión</button>
-      <button :class="{ 'step-primary': currentStep >= 8 }" class="step">Resumen General</button>
+      <button :class="{ 'step-primary': currentStep >= 7 }" class="step">Resumen General</button>
     </div>
 
     <!-- Conditional Rendering per Step -->
@@ -53,45 +52,18 @@
             <tr v-if="acta?.sesion.asistenciaMiembros?.length" v-for="miembro in acta?.sesion.asistenciaMiembros" :key="miembro.idMiembro" class="hover:bg-base-100">
               <td>{{ miembro.cargo }}</td>
               <td>{{ miembro.nombre }}</td>
-              <td>{{ miembro.estadoAsistencia === 'ASISTIÓ' ? '✔' : '' }}</td>
+              <td>{{ miembro.estadoAsistencia === 'ASISTIO' ? '✔' : '' }}</td>
               <td>{{ miembro.estadoAsistencia === 'NO ASISTIÓ' ? '✖' : '' }}</td>
                 <span v-if="miembro.estadoAsistencia === 'EXCUSA'">
                   ⚠️ {{ miembro.excusa || 'Sin excusa proporcionada' }}
                 </span>
-                <span v-else>{{ miembro.estadoAsistencia }}</span>
             </tr>
             <tr v-else>
               <td colspan="5" class="text-center">No hay datos de quórum disponibles</td>
             </tr>
           </tbody>
         </table>
-        <table class="table w-full rounded-lg bg-base-200 shadow-lg mt-4">
-          <thead class="bg-primary text-primary-content">
-            <tr>
-              <th>Dependencia</th>
-              <th>Nombre</th>
-              <th>Asistió</th>
-              <th>No Asistió</th>
-              <th>Excusa</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-if="acta?.sesion.asistenciaInvitados?.length" v-for="invitado in acta?.sesion.asistenciaInvitados" :key="invitado.idInvitado" class="hover:bg-base-100">
-              <td>{{ invitado.dependencia }}</td>
-              <td>{{ invitado.nombre }}</td>
-              <td>{{ invitado.estadoAsistencia === 'ASISTIÓ' ? '✔' : '' }}</td>
-              <td>{{ invitado.estadoAsistencia === 'NO ASISTIÓ' ? '✖' : '' }}</td>
-              <td>
-                <span v-if="invitado.estadoAsistencia === 'EXCUSA'">
-                  ⚠️ {{ invitado.excusa || 'Sin excusa proporcionada' }}
-                </span>
-              </td>
-            </tr>
-            <tr v-else>
-              <td colspan="5" class="text-center">No hay datos de asistencia para los invitados</td>
-            </tr>
-          </tbody>
-        </table>
+       
 
       </div>
 
@@ -115,15 +87,16 @@
           <ListBulletIcon class="w-6 h-6" /> 
           <span>Orden del Día</span>
         </h2>
-        <ul v-if="acta?.sesion.contenido">
-          <li>1. Verificación del Quórum</li>
-          <li>2. Aprobación del Orden del Día</li>
-          <li>3. Aprobación del Acta Anterior</li>
-          <li v-for="(punto, index) in acta?.sesion.contenido.split(';')" :key="index">
-            {{ index + 4 }}. {{ punto || 'Punto sin especificar' }}
+        <ul v-if="typeof acta?.sesion.contenido === 'string'">
+          <li 
+            v-for="(punto, index) in acta.sesion.contenido.split(/\n/)
+              .map(item => item.trim()) 
+              .filter(item => item !== '') "      
+              :key="index" class="mb-2">
+            {{ punto || 'Punto sin especificar' }}
           </li>
         </ul>
-        <p v-else class="text-center">No hay puntos en el orden del día</p>
+        <p v-else class="text-gray-500">No hay contenido definido para el orden del día.</p>
       </div>
 
 
